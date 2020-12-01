c53369df84949d688b62ff375b5f665d2da67dd3
Mattia Centini <mattia.centini@gmail.com>
2020-12-01 08:59:00 +0100

    added frame, adjusted header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import logo from '../media/Mrblack-logo.png'
 import IconButton from '@material-ui/core/IconButton';
 import {Link} from 'react-router-dom'
 import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 import owl from '../media/owl.png'
 
 function Header() {
@@ -14,7 +15,7 @@ function Header() {
                 <IconButton size='small'><Link to='/gallery'><p>Gallery</p></Link></IconButton>
                 <IconButton size='small'><Link tp='/menu'><p>Menu</p></Link></IconButton>
                 <IconButton size='small'><Link to='/contact'><p>Contact</p></Link></IconButton>
-                <IconButton size='small'><Link to='/offer'><p>Offer</p></Link></IconButton>
+                {/* <IconButton size='small'><Link to='/offer'><p>Offer</p></Link></IconButton> */}
             </div>
             <div className='header__center'>
                 <Link to='/'>
@@ -23,7 +24,8 @@ function Header() {
             </div>
             <div className='header__right'>
                 <IconButton size='large'><a className='header__rightAnchor' href="https://www.facebook.com/bar.mr.black/" rel='noopener' target='_blank'><FacebookIcon className='facebook' size='large'/></a></IconButton>
-                <IconButton size='small'><a className='header__rightAnchor' href="https://www.tripadvisor.com/Restaurant_Review-g274772-d15150528-Reviews-Mr_Black_Cocktail_Bar-Krakow_Lesser_Poland_Province_Southern_Poland.html" rel='noopener' target='_blank'><img src={owl} height='40px' width='40px'/></a></IconButton>
+                <IconButton size='large'><a className='header__rightAnchor' href="https://www.instagram.com/bar.mr.black/" rel='noopener' target='_blank'><InstagramIcon className='facebook' size='large'/></a></IconButton>
+                <IconButton size='small'><a className='header__rightAnchor' href="https://www.tripadvisor.com/Restaurant_Review-g274772-d15150528-Reviews-Mr_Black_Cocktail_Bar-Krakow_Lesser_Poland_Province_Southern_Poland.html" rel='noopener' target='_blank'><img className='header__rightTA' src={owl} /></a></IconButton>
                 
             </div>
         </div>
